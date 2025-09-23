9cd93beda3fa3b6996e794ef9c0e006b922e8ce7
Bigblockkak <fillar.vincent02@gmail.com>
2025-09-23 14:12:21 +0800

    new

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -30,7 +30,7 @@ export default function Contact() {
     setStatus("");
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/send-email`, {
+      const res = await fetch(`https://myportfoliobackend-1-uyui.onrender.com/api/send-email`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
